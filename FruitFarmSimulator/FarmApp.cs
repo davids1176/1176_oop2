16dea2e41f1142ec849a30631b1cc5348eebb1ae
davids1176 <1176@student.itgymnazium.cz>
2024-05-16 13:51:55 +0200

    Init

---
+++ FruitFarmSimulator/FarmApp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace FruitFarmSimulator
+{
+    class FarmApp
+    {
+        static string[] plants = { "apple", "peach", "pear", "plum", "cherry" };
+    
+        static void Run()
+        {
+            Orchard[] orchards = new Orchard[20];
+            int[] yields = new int[orchards.Length];
+            int[] plantYields = new int[plants.Length];
+
+            for (int i = 0; i < orchards.Length; i++)
+            {
+                Random rand = new Random();
+                int plantId = rand.Next(0, plants.Length);
+                Orchard orchard = new Orchard(rand.Next(0, 20), rand.Next(0, 20), plants[plantId], rand.Next(0, 20), rand.Next(40, 100));
+                orchards[i] = orchard;
+                yields[i] = orchard.Harvest();
+                plantYields[plantId] = yields[i];
+            }
+
+            for (int i = 0; i < plants.Length; i++)
+            {
+                Console.WriteLine(plants[i] + ": " + plantYields[i] + " Kg");
+            }
+        }
+
+        public static int SafelyConvertToInt(string x)
+        {
+            if (int.TryParse(x, out int result))
+            {
+                return result;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Run();
+        }
+    }
+}
---
+++ FruitFarmSimulator/Orchard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace FruitFarmSimulator
+{
+    public class Orchard
+    {
+        public int length { get; }
+        public int width { get; }
+        public string plant { get; }
+        public int yieldMin { get; }
+        public int yieldMax { get; }
+
+        public int Harvest()
+        {
+            Random rand = new Random();
+            int sq = length * width;
+            return sq * rand.Next(yieldMin, yieldMax);
+        }
+
+        public Orchard(int length, int width, string plant, int yieldMin, int yieldMax)
+        {
+            this.length = length;
+            this.width = width;
+            this.plant = plant;
+            this.yieldMin = yieldMin;
+            this.yieldMax = yieldMax;
+        }
+    }
+}
